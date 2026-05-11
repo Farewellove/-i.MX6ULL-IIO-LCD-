49ac044572259c56a8e0bbd17d81b98225a7b4c3
Farewellove <why15695111650@gmail.com>
2026-05-11 22:15:44 +0800

    feat: 升级 ICM20608 SPI 驱动为 IIO 框架，增加多通道 sysfs 节点，read_raw 回调

--- kernel_drivers/icm20608_iio/icm20608.c
+++ kernel_drivers/icm20608_iio/icm20608.c
@@ -2,7 +2,7 @@
  * @Author: Farewellove
  * @Date: 2026/4/21 15:02:44
  * @LastEditors: Farewellove
- * @LastEditTime: 2026/5/11 21:15:46
+ * @LastEditTime: 2026/5/11 22:01:24
  * @Description:
  * @Copyright: Copyright (©)}) 2026 Farewellove. All rights reserved.
  * @Email: 183085452@qq.com
@@ -14,6 +14,8 @@
 #include "icm20608reg.h"
 #include <linux/delay.h>
 #include <linux/slab.h>
+#include <linux/iio/iio.h>
+#include <linux/iio/sysfs.h>
 
 static struct icm20608_dev
 {
@@ -42,6 +44,16 @@ static int icm20608_write_reg(struct icm20608_dev *dev, u8 reg, u8 val)
     tx[1] = val;
     return spi_write(dev->spi, tx, 2);
 }
+
+static const struct iio_chan_spec icm20608_channels[] = {
+    {.type = IIO_ACCEL, .indexed = 1, .channel = 0, .info_mask_separate = BIT(IIO_CHAN_INFO_RAW)},
+    {.type = IIO_ACCEL, .indexed = 1, .channel = 1, .info_mask_separate = BIT(IIO_CHAN_INFO_RAW)},
+    {.type = IIO_ACCEL, .indexed = 1, .channel = 2, .info_mask_separate = BIT(IIO_CHAN_INFO_RAW)},
+    {.type = IIO_ANGL_VEL, .indexed = 1, .channel = 0, .info_mask_separate = BIT(IIO_CHAN_INFO_RAW)},
+    {.type = IIO_ANGL_VEL, .indexed = 1, .channel = 1, .info_mask_separate = BIT(IIO_CHAN_INFO_RAW)},
+    {.type = IIO_ANGL_VEL, .indexed = 1, .channel = 2, .info_mask_separate = BIT(IIO_CHAN_INFO_RAW)},
+    {.type = IIO_TEMP, .indexed = 1, .channel = 0, .info_mask_separate = BIT(IIO_CHAN_INFO_RAW)},
+};
 
 // 六轴 + 温度读取函数
 static int read_accel_x(struct icm20608_dev *dev)
@@ -101,17 +113,56 @@ static int read_temp(struct icm20608_dev *dev)
     return (h << 8) | l;
 }
 
+static int icm20608_read_raw(struct iio_dev *indio_dev,
+                             struct iio_chan_spec const *chan,
+                             int *val, int *val2, long mask)
+{
+    struct icm20608_dev *data = iio_priv(indio_dev);
+
+    if (mask != IIO_CHAN_INFO_RAW)
+        return -EINVAL;
+
+    switch (chan->type)
+    {
+    case IIO_ACCEL:
+        if (chan->channel == 0)
+            *val = read_accel_x(data);
+        if (chan->channel == 1)
+            *val = read_accel_y(data);
+        if (chan->channel == 2)
+            *val = read_accel_z(data);
+        break;
+    case IIO_ANGL_VEL:
+        if (chan->channel == 0)
+            *val = read_gyro_x(data);
+        if (chan->channel == 1)
+            *val = read_gyro_y(data);
+        if (chan->channel == 2)
+            *val = read_gyro_z(data);
+        break;
+    case IIO_TEMP:
+        *val = read_temp(data);
+        break;
+    default:
+        return -EINVAL;
+    }
+
+    return IIO_VAL_INT;
+}
+
 static int icm20608_probe(struct spi_device *spi)
 {
-    struct icm20608_dev *dev;
-    int id, ret, val;
+    int ret, id;
+    struct iio_dev *indio_dev;
+    struct icm20608_dev *data;
 
-    dev = kzalloc(sizeof(*dev), GFP_KERNEL);
-    if (!dev)
+    // 创建 IIO 设备并分配私有数据
+    indio_dev = iio_device_alloc(sizeof(*data));
+    if (!indio_dev)
         return -ENOMEM;
 
-    dev->spi = spi;
-    spi_set_drvdata(spi, dev);
+    data = iio_priv(indio_dev);
+    data->spi = spi; // 保存 SPI 指针
 
     // SPI 初始化
     spi->mode = SPI_MODE_0;
@@ -121,71 +172,80 @@ static int icm20608_probe(struct spi_device *spi)
     if (ret < 0)
     {
         printk("SPI setup failed: %d\n", ret);
-        kfree(dev);
+        kfree(data);
+        iio_device_free(indio_dev);
         return ret;
     }
 
     printk("icm20608 probe start\n");
 
     // 复位芯片
-    ret = icm20608_write_reg(dev, ICM20608_PWR_MGMT_1, 0x80);
-    if (ret < 0)
-        printk("write PWR_MGMT_1 reset failed: %d\n", ret);
+    icm20608_write_reg(data, ICM20608_PWR_MGMT_1, 0x80);
     msleep(50);
 
     // 退出休眠，选择时钟源
-    ret = icm20608_write_reg(dev, ICM20608_PWR_MGMT_1, 0x01);
-    if (ret < 0)
-        printk("write PWR_MGMT_1 wake failed: %d\n", ret);
+    icm20608_write_reg(data, ICM20608_PWR_MGMT_1, 0x01);
     msleep(150);
 
     // WHO_AM_I 验证
-    id = icm20608_read_reg(dev, ICM20608_WHO_AM_I);
-    if (id < 0)
-    {
-        printk("read WHO_AM_I failed\n");
-        kfree(dev);
-        return id;
-    }
-    printk("WHO_AM_I = 0x%x\n", id);
+    id = icm20608_read_reg(data, ICM20608_WHO_AM_I);
+    printk("ICM20608 WHO_AM_I = 0x%x\n", id);
     if (id != ICM20608G_ID && id != ICM20608D_ID)
     {
-        printk("id check error\n");
-        kfree(dev);
+        printk("ICM20608 id check error\n");
+        kfree(data);
+        iio_device_free(indio_dev);
         return -ENODEV;
     }
-    printk("id check success\n");
+    printk("ICM20608 id check success\n");
 
-    // 配置采样率和滤波
-    icm20608_write_reg(dev, ICM20608_SMPLRT_DIV, 0x00);
-    icm20608_write_reg(dev, ICM20608_GYRO_CONFIG, 0x18);   // ±2000 dps
-    icm20608_write_reg(dev, ICM20608_ACCEL_CONFIG, 0x18);  // ±16g
-    icm20608_write_reg(dev, ICM20608_CONFIG, 0x04);        // 陀螺仪低通滤波 BW=20Hz
-    icm20608_write_reg(dev, ICM20608_ACCEL_CONFIG2, 0x04); // 加速度低通滤波 BW=21.2Hz
-
-    // 开启所有轴
-    icm20608_write_reg(dev, ICM20608_PWR_MGMT_2, 0x00);
-    icm20608_write_reg(dev, ICM20608_LP_MODE_CFG, 0x00); // 关闭低功耗
-    icm20608_write_reg(dev, ICM20608_FIFO_EN, 0x00);     // 关闭 FIFO
+    // 正点原子初始化顺序
+    icm20608_write_reg(data, ICM20608_SMPLRT_DIV, 0x00);
+    icm20608_write_reg(data, ICM20608_GYRO_CONFIG, 0x18);
+    icm20608_write_reg(data, ICM20608_ACCEL_CONFIG, 0x18);
+    icm20608_write_reg(data, ICM20608_CONFIG, 0x04);
+    icm20608_write_reg(data, ICM20608_ACCEL_CONFIG2, 0x04);
+    icm20608_write_reg(data, ICM20608_PWR_MGMT_2, 0x00);
+    icm20608_write_reg(data, ICM20608_LP_MODE_CFG, 0x00);
+    icm20608_write_reg(data, ICM20608_FIFO_EN, 0x00);
     msleep(100);
+
+    // 配置 IIO device
+    indio_dev->name = "icm20608";
+    indio_dev->channels = icm20608_channels;
+    indio_dev->num_channels = ARRAY_SIZE(icm20608_channels);
+    indio_dev->info = &(struct iio_info){
+        .read_raw = icm20608_read_raw,
+    };
+    indio_dev->modes = INDIO_DIRECT_MODE;
+
+    // **绑定到 SPI**
+    spi_set_drvdata(spi, indio_dev);
+
+    // 注册 IIO 设备
+    ret = iio_device_register(indio_dev);
+    if (ret < 0)
+    {
+        printk("iio_device_register failed: %d\n", ret);
+        iio_device_free(indio_dev);
+        return ret;
+    }
 
     // 读取六轴 + 温度打印
     printk("Accel: X=%d Y=%d Z=%d | Gyro: X=%d Y=%d Z=%d | Temp=%d\n",
-           read_accel_x(dev), read_accel_y(dev), read_accel_z(dev),
-           read_gyro_x(dev), read_gyro_y(dev), read_gyro_z(dev),
-           read_temp(dev));
-
+           read_accel_x(data), read_accel_y(data),
+           read_accel_z(data), read_gyro_x(data),
+           read_gyro_y(data), read_gyro_z(data), read_temp(data));
     printk("icm20608 probe finished\n");
-
     return 0;
 }
 
 static int icm20608_remove(struct spi_device *spi)
 {
-
-    struct icm20608_dev *dev = spi_get_drvdata(spi);
-    kfree(dev);
-    printk("icm20608 remove\r\n");
+    struct iio_dev *indio_dev = spi_get_drvdata(spi);
+    iio_device_unregister(indio_dev);
+    kfree(iio_priv(indio_dev));
+    printk("icm20608 remove\n");
     return 0;
 }
 
